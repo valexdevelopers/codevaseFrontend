0fae14aa8429a09a585f80654e8001d2244a38a3
Johnnyxcode <john.ayo.jam@gmail.com>
2024-06-18 13:21:39 +0100

    pulled again from main

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import SideBar from './partials/SideBar'
-import HomeContent from './partials/HomeContent'
-import '../assets/styles/homepage.styles.css'
-import '../assets/styles/custom.css'
+import React from "react";
+import SideBar from "./partials/SideBar";
+import HomeContent from "./partials/HomeContent";
+import "../assets/styles/homepage.styles.css";
+import "../assets/styles/custom.css";
 
 function Home() {
   return (
     <div className="home">
-        <SideBar /> 
-        <HomeContent/>
+      <SideBar />
+      <HomeContent />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/components/partials/SideBar.jsx
+++ src/components/partials/SideBar.jsx
@@ -1,31 +1,41 @@
-import React from 'react'
-import  logo from '../../assets/images/logo/logo.png';
-import '../../assets/styles/homeSiderBar.css'
+import React from "react";
+import logo from "../../assets/images/logo/logo.png";
+import "../../assets/styles/homeSiderBar.css";
 function SideBar() {
   return (
-
-	<div className="side_bar_content">
-		<div className="nav_brand">
-			<img src={logo} alt=""  />
-		</div>
-		<div className="try_editor">
-			<p>Try Our Online Editor</p>
-		</div>
-		<div className="animated_button">
-			<a href="" className="animated_coding_link">Start Coding</a>
-		</div>
-		<div className="side_bar_menu">
-			<ul>
-				<li className="side_bar_menu_item"><a href="/search" className="side_bar_menu_link">Search Pens</a></li>
-				<li className="side_bar_menu_item"><a href="/codingEditor" className="side_bar_menu_link">Challenges</a></li>
-				<li className="side_bar_menu_item"><a href="#" className="side_bar_menu_link">Sparks</a></li>
-			</ul>
-		</div>
-		
-	</div>
-		
-
-  )
+    <div className="side_bar_content">
+      <div className="nav_brand">
+        <img src={logo} alt="" />
+      </div>
+      <div className="try_editor">
+        <p>Try Our Online Editor</p>
+      </div>
+      <div className="animated_button">
+        <a href="" className="animated_coding_link">
+          Start Coding
+        </a>
+      </div>
+      <div className="side_bar_menu">
+        <ul>
+          <li className="side_bar_menu_item">
+            <a href="/search" className="side_bar_menu_link">
+              Search Pens
+            </a>
+          </li>
+          <li className="side_bar_menu_item">
+            <a href="/codingEditor" className="side_bar_menu_link">
+              Challenges
+            </a>
+          </li>
+          <li className="side_bar_menu_item">
+            <a href="#" className="side_bar_menu_link">
+              Sparks
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default SideBar
+export default SideBar;
